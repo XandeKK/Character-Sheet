3a989c438330d8d6883babcd5f239b1c1c9a40bd
Alenxadre <alexandre.dosa@gmail.com>
2022-08-15 15:03:33 -0300

    Add local env

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,18 @@ module CharacterSheet
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # I18n
     I18n.available_locales = [:en, :"pt-BR"]
-    # Set default locale to something other than :en
     I18n.default_locale = :en
+
+    # Environment Variables
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
 
     # Configuration for the application, engines, and railties goes here.
     #
