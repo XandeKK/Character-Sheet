aa03393058525d2cc02087cc8639bb4530c1d988
Alenxadre <alexandre.dosa@gmail.com>
2022-08-16 14:04:55 -0300

    Add env mailer sender

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = ENV["MAILER_SENDER"]
   config.rotate_csrf_on_sign_in = true
   config.signed_cookie = true
 end
