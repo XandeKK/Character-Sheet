abc4e9d1b453323be386333d11f995ab495b1486
Alenxadre <alexandre.dosa@gmail.com>
2022-09-13 10:10:46 -0300

    Style show characters view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import scrollSpy from 'simple-scrollspy'
+
+window.addEventListener("turbo:load", function() {
+  const mainMenu = document.getElementById('main-menu');
+  if (mainMenu) {
+    const options = {
+      sectionClass: '.scrollspy',
+      menuActiveTarget: '.menu-item',
+      offset: 100,
+      hrefAttribute: "href-spy",
+      activeClass: "fill-rose-500"
+    }
+
+    scrollSpy(mainMenu, options);
+  }  
+})
+
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,9 +9,27 @@ module.exports = {
     './app/views/**/*.{erb,html}'
   ],
   theme: {
+    screens: {
+      'mm': '320px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        pathfinder: ['Pathfinder'],
       },
     },
   },
