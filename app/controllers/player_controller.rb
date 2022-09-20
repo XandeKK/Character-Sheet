defe033e6ac9afcfe92ed84c2f72f50361e3eda9
Alenxadre <alexandre.dosa@gmail.com>
2022-09-20 17:03:42 -0300

    Put to to get only character player

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,7 +2,8 @@ class PlayerController < ApplicationController
   def index
     @characters = current_user.characters
       .select("characters.id, pathfinder_basics.name")
-      .joins(:pathfinder_basic)
+      .joins(:character_category, :pathfinder_basic)
+      .where("character_category.name": "Player")
       .includes(character_image_attachment: :blob)
   end
 end
