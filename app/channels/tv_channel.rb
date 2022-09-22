3e93a5d37c10050537dd7e56beae37d328dc7aba
Alenxadre <alexandre.dosa@gmail.com>
2022-09-22 13:24:59 -0300

    Add channel in tv

--- app/channels/tv_channel.rb
+++ app/channels/tv_channel.rb
@@ -1,6 +1,5 @@
 class TvChannel < ApplicationCable::Channel
   def subscribed
-    p params
     if Adventure.find_by(server_name: params[:server_name])&.authenticate(params[:password])
       stream_from "tv_#{params[:server_name]}"
     else 
