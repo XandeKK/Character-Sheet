b68ede62ff1183debd8d01f3869e68e4ac9f82b8
Alenxadre <alexandre.dosa@gmail.com>
2022-09-24 21:46:42 -0300

    Improve adventure participation controller test

--- test/controllers/adventure_participation_controller_test.rb
+++ test/controllers/adventure_participation_controller_test.rb
@@ -5,7 +5,7 @@ class AdventureParticipationControllerTest < ActionDispatch::IntegrationTest
     sign_up
 
     assert_difference("AdventureParticipation.count") do
-      post adventure_participation_path, params: {
+      post adventure_participation_path(format: :turbo_stream), params: {
         character_id: characters(:one).id,
         adventure_id: adventures(:two).id
       }
@@ -13,15 +13,87 @@ class AdventureParticipationControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "shouldn't create adventure participation with character id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      post adventure_participation_path(format: :turbo_stream), params: {
+        character_id: - 302,
+        adventure_id: adventures(:two).id
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't create adventure participation with adventure id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      post adventure_participation_path(format: :turbo_stream), params: {
+        character_id: characters(:one).id,
+        adventure_id: -231
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't create adventure participation with character id and adventure id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      post adventure_participation_path(format: :turbo_stream), params: {
+        character_id: - 302,
+        adventure_id: -2091
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
   test "should destroy adventure participation" do
     sign_up
 
     assert_difference("AdventureParticipation.count", -1) do
-      delete adventure_participation_path, params: {
+      delete adventure_participation_path(format: :turbo_stream), params: {
         character_id: characters(:one).id,
         adventure_id: adventures(:one).id
       }
     end
     assert_response :success
+  end
+
+  test "shouldn't destroy adventure participation with character id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      delete adventure_participation_path(format: :turbo_stream), params: {
+        character_id: -231,
+        adventure_id: adventures(:one).id
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't destroy adventure participation with adventure id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      delete adventure_participation_path(format: :turbo_stream), params: {
+        character_id: characters(:one).id,
+        adventure_id: -23
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't destroy adventure participation with character id and adventure id invalid" do
+    sign_up
+
+    assert_no_difference("AdventureParticipation.count") do
+      delete adventure_participation_path(format: :turbo_stream), params: {
+        character_id: -231,
+        adventure_id: -23
+      }
+    end
+    assert_response :unprocessable_entity
   end
 end
