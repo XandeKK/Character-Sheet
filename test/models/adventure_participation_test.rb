8bf7b2b4a4da50c37b174b95047406f46e4d0f02
Alenxadre <alexandre.dosa@gmail.com>
2022-09-24 21:13:58 -0300

    Improve fixture

--- test/models/adventure_participation_test.rb
+++ test/models/adventure_participation_test.rb
@@ -9,28 +9,28 @@ class AdventureParticipationTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-    assert @adventure_participation.valid?, "Did not validate with adventure valid"
+    assert @adventure_participation.valid?, "Did not validate with adventure participation valid"
   end
 
   test "should be invalid without character" do
     @adventure_participation.character = nil
-    assert @adventure_participation.invalid?, "Validated the adventure without character"
+    assert @adventure_participation.invalid?, "Validated the adventure participation without character"
   end
 
   test "should be invalid without adventure" do
     @adventure_participation.adventure = nil
-    assert @adventure_participation.invalid?, "Validated the adventure without adventure"
+    assert @adventure_participation.invalid?, "Validated the adventure participation without adventure"
   end
 
   test "should save" do
     assert_difference("AdventureParticipation.count") do
-      assert @adventure_participation.save, "Did not save with adventure valid"
+      assert @adventure_participation.save, "Did not save with adventure participation valid"
     end
   end
 
   test "should destroy" do
     assert_difference("AdventureParticipation.count", -1) do
-      assert adventure_participations(:one).destroy, "Did not destroy adventure"
+      assert adventure_participations(:one).destroy, "Did not destroy adventure participation"
     end
   end
 end
