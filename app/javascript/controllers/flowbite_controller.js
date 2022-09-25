9a37ce6c0a428dcde497caf3f11da77adad4b02c
Alenxadre <alexandre.dosa@gmail.com>
2022-09-25 14:33:32 -0300

    Adjust the macro in character

--- app/javascript/controllers/flowbite_controller.js
+++ app/javascript/controllers/flowbite_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
     "drawer_navigation", "drawer_character",
     "drawer_character_title", "drawer_character_note", "dice_div",
     "attack", "secondAttack", "thirdAttack", "damage",
+    "weaponDice", "macro",
   ]
   connect() {
     if (this.hasDrawer_navigationTarget) {
@@ -51,6 +52,26 @@ export default class extends Controller {
     this.thirdAttackTarget.textContent = thirdAttack;
 
     this.drawerRight.toggle();
+    this.macroTarget.classList.add("hidden")
+    this.weaponDiceTarget.classList.remove("hidden")
+    this.dice_divTarget.classList.remove("hidden")
+  }
+
+  toggleDrawerMacro(event) {
+    let name = event.params.name;
+    let notation = event.params.notation;
+    let note = event.params.note;
+
+    this.drawer_character_titleTarget.textContent = name
+    this.drawer_character_noteTarget.textContent = `Note: \n${note}`
+
+    this.macroTarget.textContent = notation;
+    this.macroTarget.textContent = notation;
+    this.macroTarget.setAttribute("data-channels--character-dice-param", notation);
+
+    this.drawerRight.toggle();
+    this.macroTarget.classList.remove("hidden")
+    this.weaponDiceTarget.classList.add("hidden")
     this.dice_divTarget.classList.remove("hidden")
   }
 }
