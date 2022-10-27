7fd9c7007d64c315e82055c7b67f82310eb9e310
Alenxadre <alexandre.dosa@gmail.com>
2022-10-27 11:13:18 -0300

    Remove advanced roller

--- app/javascript/controllers/tv/dice_controller.js
+++ app/javascript/controllers/tv/dice_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 import DiceBox from "@3d-dice/dice-box";
-import AdvancedRoller from "advanced-roller";
 
 export default class extends Controller {
   static targets = ["chat"]
@@ -9,7 +8,7 @@ export default class extends Controller {
     this.characters = {};
     this.charactersEvents = [];
     this.currentIndex = 0;
-    this.finalResultLength = 0;
+    this.accumulatorRoll = 0;
 
     this.box = new DiceBox("#dice-box", {
       id: "dice-canvas",
@@ -22,111 +21,58 @@ export default class extends Controller {
     this.box.init();
 
     this.box.onRollComplete = this.rollComplete.bind(this);
-
-    this.Roller = new AdvancedRoller({
-      target: "#dice-box",
-      onSubmit: (notation, theme) => this.box.add(notation, theme),
-      onClear: () => {
-        this.box.clear();
-      },
-      onReroll: (rolls) => {
-        // loop through parsed roll notations and send them to the Box
-        rolls.forEach((roll) => this.box.add(roll, roll.groupId));
-      },
-      onResults: (results) => {
-        let item = this.createItemChat(results);
-        this.chatTarget.appendChild(item);
-        this.chatTarget.scrollBy({
-          top: this.chatTarget.scrollHeight,
-          left: 0,
-          behavior: 'smooth'
-        })
-      }
-    });
   }
 
   rollDice(data) {
     this.characters[data.id] = {
       name: data.name,
       image: data.image,
-      color: data.color,
-      dice: data.dice
+      color: data.color
     };
 
-    let regex = /\d*d(4|6|8|100|10|12|20)/gi
+    this.charactersEvents.push(data.id);
 
-    let length = data.dice.match(regex).length
-
-    for (var i = 0; i < length; i++) {
-      this.charactersEvents.push({
-        id: data.id,
-        finalResultLength: this.finalResultLength,
-        dice: data.dice
-      });
-    }
-
-    this.finalResultLength += 1;
-
-    this.Roller.submit(data.dice, {
+    this.box.add(data.dice, {
       themeColor: data.color,
       theme: data.theme,
     });
   }
 
-  rollComplete(results) {
-    let previousPlayer = null
-    let currentPlayer = null
-    let groupResults = []
+  rollComplete(dieResult) {
+    for(let i = this.currentIndex; i < dieResult.length; i++) {
+      let groupId = dieResult[i].id;
+      let qty = dieResult[i].qty;
+      let item = this.createItemChat(i, dieResult[i]);
+      this.chatTarget.appendChild(item);
 
-    for (var i = this.currentIndex; i < results.length; i++) {
-      currentPlayer = this.charactersEvents[i];
+      this.removeDice(groupId, qty);
 
-      if (previousPlayer == null) {
-        previousPlayer = currentPlayer;
-      }
-
-      if (currentPlayer.id == previousPlayer.id && currentPlayer.finalResultLength == previousPlayer.finalResultLength) {
-        groupResults.push(results[i])
-      } else {
-        this.Roller.handleResults(groupResults, previousPlayer.finalResultLength);
-        groupResults = []
-        groupResults.push(results[i])
-      }
-
-      if (currentPlayer.id == previousPlayer.id && i == results.length-1) {
-        this.Roller.handleResults(groupResults, currentPlayer.finalResultLength);
-        groupResults = [] 
-      }
-
-      if (i == results.length-1 && groupResults.length > 0) {
-        this.Roller.handleResults(groupResults, currentPlayer.finalResultLength);
-      }
-      
-      previousPlayer = currentPlayer;
-      
-      this.removeDice(results[i].rolls)
-
+      this.chatTarget.scrollBy({
+        top: this.chatTarget.scrollHeight,
+        left: 0,
+        behavior: 'smooth'
+      })
     }
-
-    this.currentIndex = results.length;
+    this.currentIndex = dieResult.length;
   }
 
-  removeDice(rolls) {
-    rolls.forEach((roll)=> {
-      setTimeout(()=> {
-        this.box.remove({groupId: roll.groupId, rollId: roll.rollId})
-      }, 8000)
-    })
+  removeDice(groupId, qty) {
+    setTimeout(this.timeoutRemove.bind(this, groupId, qty), 8000); // 8 seconds
   }
+
+  timeoutRemove(groupId, qty) {
+    for (let i = this.accumulatorRoll; i < qty + this.accumulatorRoll; i++) {
+      this.box.remove({groupId: groupId, rollId: i});
+    }
+    this.accumulatorRoll += qty;
+  } 
 
   getDices({ detail: { data }}) {
     this.rollDice(data)
   }
 
-  createItemChat(results) {
-    let playerPosition = results.playerPosition;
-    let dice = this.charactersEvents[playerPosition].dice;
-    let id = this.charactersEvents[playerPosition].id;
+  createItemChat(index, dieResult) {
+    let id = this.charactersEvents[index];
     let character = this.characters[id];
 
     // Create Element
@@ -144,7 +90,7 @@ export default class extends Controller {
     let spanResult = document.createElement("span");
 
     // Set id
-    popoverDiv.id = `popover-${playerPosition}`;
+    popoverDiv.id = `popover-${index}`;
 
     // Set ClassName
     mainDiv.className = "w-full h-20 rounded-lg shadow-md p-2 flex space-x-4";
@@ -155,7 +101,6 @@ export default class extends Controller {
     img.className = "rounded-full w-16 h-16";
 
     pText.className = "text-black dark:text-white"
-    pPopoverText.className = "text-lg"
     spanName.className = "font-bold";
 
     // Set style
@@ -174,13 +119,22 @@ export default class extends Controller {
 
     // Set text Content
     spanName.textContent = `${character.name}: `;
-    spanResult.textContent = results.value;
+    spanResult.textContent = dieResult.value;
 
     img.src = character.image;
 
-    let popoverText = `<span class="font-bold">${dice}</span></br>${this.showResults(results)}`
+    let rolls = dieResult.rolls.map(function(roll) {return roll.value});
+    rolls = rolls.join(" + ");
 
-    pPopoverText.innerHTML = popoverText;
+    let popoverText = null;
+
+    if (dieResult.modifier != 0) {
+      popoverText = `dice: ${dieResult.qty}d${dieResult.sides}</br>rolls: ${rolls} | bonus: ${dieResult.modifier}`;
+    } else {
+      popoverText = `dice: ${dieResult.qty}d${dieResult.sides}</br>rolls: ${rolls}`;
+    }
+
+    pPopoverText.innerHTML = popoverText
 
     // Append element
     mainDiv.appendChild(img);
@@ -201,95 +155,5 @@ export default class extends Controller {
     const popover = new Popover(popoverDiv, spanResult);
 
     return mainDiv;
-  }
-
-  showResults(data){
-    let rolls
-    if(data.rolls && !Array.isArray(data.rolls)){
-      rolls = Object.values(data.rolls).map(roll => roll)
-    } else {
-      rolls = Object.values(this.recursiveSearch(data,'rolls')).map(group => {
-        return Object.values(group)
-      }).flat()
-    }
-
-    let total = 0
-    if(data.hasOwnProperty('value')) {
-      total = data.value
-    } else { 
-      total = rolls.reduce((val,roll) => val + roll.value,0)
-      let modifier = data.reduce((val,roll) => val + roll.modifier,0)
-      total += modifier
-    }
-
-    total = isNaN(total) ? '...' : total
-    let resultString = ''
-
-    rolls.forEach((roll,i) => {
-      let val
-      let sides = roll.die || roll.sides || 'fate'
-      if(i !== 0) {
-        resultString += ', '
-      }
-
-      // if(roll.success !== undefined && roll.success !== null){
-      //   val = roll.success ?
-      //    `<svg class="success"><use href="${checkIcon}#checkmark"></use></svg>` : roll.failures > 0 ?
-      //     `<svg class="failure"><use href="${cancelIcon}#cancel"></use></svg>` : `<svg class="null"><use href="${minusIcon}#minus"></use></svg>`
-      // } else {
-        // convert to string in case value is 0 which would be evaluated as falsy
-      val = roll.hasOwnProperty('value') ? roll.value.toString() : '...'
-      // }
-      let classes = ''
-
-      if(roll.critical === "success" || (roll.hasOwnProperty('value') && sides == roll.value)) {
-        classes += ' text-green-700'
-      }
-      if(roll.critical === "failure" || (roll.success === null && roll.hasOwnProperty('value') && roll.value <= 1 && sides !== 'fate')) {
-        classes += ' text-red-700'
-      }
-      if(roll.drop) {
-        classes += ' text-gray-600 line-through decoration-gray-700'
-      }
-      if(roll.reroll) {
-        classes += ' text-gray-600 line-through decoration-gray-700'
-      }
-      if(roll.explode) {
-        classes += ' text-green-700 after:content-["!"]'
-      }
-      if(sides === 'fate'){
-        if(roll.value === 1){
-          classes += ' text-green-700'
-        }
-        if(roll.value === -1){
-          classes += ' text-red-700'
-        }
-      }
-
-      if(classes !== ''){
-        val = `<span class='font-bold ${classes.trim()}'>${val}</span>`
-      }
-
-      resultString += val
-    })
-    resultString += ` = <strong class="font-bold">${total}</strong>`
-
-    return resultString
-  }
-
-  recursiveSearch(obj, searchKey, results = [], callback) {
-    const r = results;
-    Object.keys(obj).forEach(key => {
-      const value = obj[key];
-      if(key === searchKey){
-        r.push(value);
-        if(callback && typeof callback === 'function') {
-          callback(obj)
-        }
-      } else if(value && typeof value === 'object'){
-        this.recursiveSearch(value, searchKey, r, callback);
-      }
-    });
-    return r;
   }
 }
