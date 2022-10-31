5fa0caa248021bbf8031e8d6a967d3a704a2d50c
xandekk <alexandre.dosa@gmail.com>
2022-10-31 20:51:35 -0300

    Refactor field class HTML basics and defense views

--- app/helpers/input_helper.rb
+++ app/helpers/input_helper.rb
@@ -1,4 +1,32 @@
 module InputHelper
+  def read_only
+    "bg-gray-300 dark:bg-gray-700"
+  end
+
+  def write_only
+    "bg-gray-50 dark:bg-transparent"
+  end
+
+  def normal_field
+    "border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+  end
+
+  def normal_select
+    "bg-gray-50 dark:bg-zinc-800"
+  end
+
+  def rounded_left
+    "rounded-none rounded-l-lg"
+  end
+
+  def rounded_none
+    "rounded-none"
+  end
+
+  def rounded_right
+    "rounded-none rounded-r-lg"
+  end
+
   def peer_field(model=nil, name=nil)
     if model.nil? || model.errors.messages[name].empty?
       "block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
