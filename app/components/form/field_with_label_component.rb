6fcb168f13a6a5983a5f7d475fe877be54caf4ca
xandekk <alexandre.dosa@gmail.com>
2022-11-06 16:58:09 -0300

    Add Form component

---
+++ app/components/form/field_with_label_component.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+class Form::FieldWithLabelComponent < ViewComponent::Base
+  include InputHelper
+  def initialize(form:, name:, surname:nil, read_only:nil, rounded:nil,
+    class_div:nil, form_type:"text", array_select:[])
+    @form = form
+    @name = name
+    @surname = surname
+    @read_only = read_only
+    @rounded = rounded
+    @class_div = class_div
+    @form_type = form_type
+    @array_select = array_select
+  end
+
+  private
+
+  def read_only?
+    @read_only
+  end
+
+  def rounded?
+    return "" unless @rounded
+
+    case @rounded
+     when "left"
+      "rounded-none rounded-l-lg"
+     when "right"
+      "rounded-none rounded-r-lg"
+     when "none"
+       "rounded-none"
+     end
+  end
+
+  def make_form
+    case @form_type
+    when "number"
+      make_form_number
+    when "area"
+      make_form_area
+    when "select"
+      make_form_select
+    when "text"
+      make_form_text
+    end
+  end
+
+  def make_form_text
+    if read_only?
+      @form.text_field(@name, class: "#{rounded?} #{normal_field} bg-gray-300 dark:bg-gray-700", readonly: "")
+    else
+      @form.text_field(@name, class: "#{rounded?} #{normal_field} bg-gray-50 dark:bg-transparent")
+    end
+  end
+
+  def make_form_area
+    if read_only?
+      @form.text_area(@name, class: "#{rounded?} #{normal_field} bg-gray-300 dark:bg-gray-700", readonly: "", rows: 3)
+    else
+      @form.text_area(@name, class: "#{rounded?} #{normal_field} bg-gray-50 dark:bg-transparent", rows: 3)
+    end
+  end
+
+  def make_form_number
+    if read_only?
+      @form.number_field(@name, class: "#{rounded?} #{normal_field} bg-gray-300 dark:bg-gray-700", readonly: "")
+    else
+      @form.number_field(@name, class: "#{rounded?} #{normal_field} bg-gray-50 dark:bg-transparent")
+    end
+  end
+
+  def make_form_select
+    if read_only?
+      @form.select(@name, @array_select, {}, class: "#{rounded?} #{normal_field} bg-gray-300 dark:bg-gray-700", readonly: "")
+    else
+      @form.select(@name, @array_select, {}, class: "#{rounded?} #{normal_field} bg-gray-50 dark:bg-zinc-800")
+    end
+  end
+end
