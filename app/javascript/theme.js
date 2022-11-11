63d18178a5f22d4ae40b375cc58cb971d08f5239
xandekk <alexandre.dosa@gmail.com>
2022-11-11 14:52:24 -0300

    remove loading

--- app/javascript/theme.js
+++ app/javascript/theme.js
@@ -1,11 +1,5 @@
-// Change theme
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark');
 } else {
   document.documentElement.classList.remove('dark')
 }
-
-// Remove screen loading
-window.addEventListener("turbo:load", ()=>{
-	document.getElementById("loading").remove();
-});
