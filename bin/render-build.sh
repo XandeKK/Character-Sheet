1d4fe8c9b2d6809332316c86bc080bef5a12fafc
xandekk <alexandre.dosa@gmail.com>
2022-11-13 18:08:51 -0300

    Add more tables to destroy and change develpment to production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rails runner "User.destroy_all"
+bundle exec rails runner "User.destroy_all; CharacterCategory.destroy_all; CharacterSystem.destroy_all;"
 bundle exec rake db:seed
